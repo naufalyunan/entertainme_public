15c99834c6999d443116e25518c2da9b52f4b0c4
naufalyunan <naufalyunan45@gmail.com>
2020-04-09 18:28:20 +0700

    add edit for movies and series

--- orchestrator/controllers/Controller.js
+++ orchestrator/controllers/Controller.js
@@ -8,7 +8,7 @@ class Controller {
 		const urlTwo = 'http://localhost:3001/series'
 		const requestOne = axios.get(urlOne)
 		const requestTwo = axios.get(urlTwo)
-
+		
 		redis.get('list')
 			.then((reply) => {
 				if (reply) {
@@ -32,16 +32,116 @@ class Controller {
 									res.status(200).json(payload)
 								})
 								.catch(err => {
-									res.status(500).json(err)
+									console.log('*******')
+									console.log('cathc atas')
+									res.status(500).json(err.message)
 								})
 						}))
+						.catch(err => {
+							console.log('-------')
+							console.log('catch tengah')
+							res.status(500).json(err.message)
+						})
 				}
 			})
 			.catch(err => {
-				res.status(500).json(err)
+				console.log('======')
+				console.log('catch bawah')
+				res.status(500).json(err.message)
 			})
 
 	}
+
+	static updateMovie (req, res, next) {
+		const id = req.params.id
+		const  { title, overview, poster_path, popularity, tags } = req.body
+		const payload = {
+			title,
+			overview,
+			poster_path,
+			popularity,
+			tags
+		}
+		let updated 
+		axios({
+			method: 'PATCH',
+			url: `http://localhost:3000/movies/${id}`,
+			data: payload
+		})
+			.then(result => {
+				updated = result.data
+				return redis.flushall()
+			})
+			.then(reply => {
+				const urlOne = 'http://localhost:3000/movies'
+				const urlTwo = 'http://localhost:3001/series'
+				const requestOne = axios.get(urlOne)
+				const requestTwo = axios.get(urlTwo)
+				return axios.all([requestOne, requestTwo])
+			})
+			.then(axios.spread((...responses) => {
+				const responseOne = responses[0].data
+				const responseTwo = responses[1].data
+				const payload = {
+					movies: responseOne,
+					series: responseTwo
+				}
+				return redis.set('list', JSON.stringify(payload))
+			}))
+			.then(result => {
+				res.status(200).json(updated)
+			})
+			.catch(err => {
+				console.log(err)
+				res.status(400).json(err)
+			})
+	}
+
+	static updateSeries (req, res, next) {
+		const id = req.params.id
+		const  { title, overview, poster_path, popularity, tags } = req.body
+		const payload = {
+			title,
+			overview,
+			poster_path,
+			popularity,
+			tags
+		}
+		let updated 
+		axios({
+			method: 'PATCH',
+			url: `http://localhost:3001/series/${id}`,
+			data: payload
+		})
+			.then(result => {
+				updated = result.data
+				return redis.flushall()
+			})
+			.then(reply => {
+				const urlOne = 'http://localhost:3000/movies'
+				const urlTwo = 'http://localhost:3001/series'
+				const requestOne = axios.get(urlOne)
+				const requestTwo = axios.get(urlTwo)
+				return axios.all([requestOne, requestTwo])
+			})
+			.then(axios.spread((...responses) => {
+				const responseOne = responses[0].data
+				const responseTwo = responses[1].data
+				const payload = {
+					movies: responseOne,
+					series: responseTwo
+				}
+				return redis.set('list', JSON.stringify(payload))
+			}))
+			.then(result => {
+				res.status(200).json(updated)
+			})
+			.catch(err => {
+				console.log(err)
+				res.status(400).json(err)
+			})
+	}
+
 }
 
 module.exports = Controller
--- orchestrator/routes/index.js
+++ orchestrator/routes/index.js
@@ -6,5 +6,7 @@ router.get('/', (req, res) => {
 	res.send('ORCHESTRATOR HOMEPAGE')
 })
 router.get('/entertainme', Controller.getAll)
+router.patch('/entertainme/movies/:id', Controller.updateMovie)
+router.patch('/entertainme/series/:id', Controller.updateSeries)
 
 module.exports = router
